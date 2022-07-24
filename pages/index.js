4f319761c7c4470a0019d61371a3534cf2cfae0c
MrKenobii <anldygc35@windowslive.com>
2022-07-24 14:15:16 +0300

    Products

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Layout from "../components/Layout";
+import Product from "../components/Product";
+import data from "../utils/data";
 
 export default function Home() {
   return (
     <div>
-        <Layout title="Home Page">Home Page</Layout>
+        <Layout title="Home Page">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+            {data.products.map(product => (
+              <Product product={product} key={product.slug} />
+            ))}
+          </div>
+        </Layout>
     </div>
   )
 }
